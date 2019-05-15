98199411ceef55dd15261405bf3f7207f7921fa8
Raefe Newton-Jones <raefe.nj@gmail.com>
2019-05-15 15:13:10 +0100

    added pytest

---
+++ src/problem_1.py
@@ -0,0 +1,9 @@
+a = []
+
+def sums(n):
+    for i in range(n):
+        a.append(i + 1)
+
+    print sum(a)
+
+sums(10)
---
+++ test/problem_1_test.py
@@ -0,0 +1,7 @@
+from os import path
+from pathlib import Path
+from src import problem_1
+
+
+
+def sum()
