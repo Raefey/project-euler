aa42a6e52fcd7969ae592d801f424d374613e698
Raefe Newton-Jones <raefe.nj@gmail.com>
2019-05-16 16:53:10 +0100

    problem_1_completed

--- src/problem_1.py
+++ src/problem_1.py
@@ -1,6 +1,12 @@
 a = []
 
+
 def sum_of_multiples_of_3_and_5(n):
     for i in range(n):
-        a.append(i + 1)
+        if divisible_by_3_or_5(i):
+            a.append(i)
     return sum(a)
+
+
+def divisible_by_3_or_5(number):
+    return number % 5 == 0 or number % 3 == 0
--- test/test_problem_1.py
+++ test/test_problem_1.py
@@ -1,4 +1,8 @@
+from src.problem_1 import divisible_by_3_or_5
 from src.problem_1 import sum_of_multiples_of_3_and_5
 
-def test_answer():
-    assert sum_of_multiples_of_3_and_5(10) == 55
+def test_sum():
+    assert sum_of_multiples_of_3_and_5(1000) == 233168
+
+def test_divisor():
+    assert divisible_by_3_or_5(5) is True
